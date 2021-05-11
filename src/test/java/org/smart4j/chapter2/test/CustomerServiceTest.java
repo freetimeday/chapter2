0a37bbc939c59667a56342fe9d432ce86941c399
freetimeday <836231173@qq.com>
2021-05-11 23:16:49 +0800

    test2

---
+++ src/test/java/org/smart4j/chapter2/test/CustomerServiceTest.java
@@ -0,0 +1,68 @@
+
+
+package org.smart4j.chapter2.test;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest(){
+        customerService=new CustomerService();
+    }
+
+    @Before
+    public void init(){
+        //TODO初始化数据库
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception{
+        List<Customer> customerList= customerService.getCustomerList("jack");
+        Assert.assertEquals(2,customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() throws Exception{
+        Customer customer = customerService.getCustomer(1);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception{
+
+        Map<String,Object> fieldMap=new HashMap<String, Object>();
+        fieldMap.put("name","customer100");
+        fieldMap.put("contact","tom");
+        fieldMap.put("telephone","18722322222");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception{
+        HashMap<String, Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("contact","kris");
+        boolean result = customerService.updateCustomer(1, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    public void deleteCustomerTest() throws Exception{
+        long id=1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+
+    }
+
+}
