f477de0021d5529f2f5caa3eeef894ad7dfefc26
freetimeday <836231173@qq.com>
2021-05-13 23:23:42 +0800

    test2

--- src/main/java/org/smart4j/chapter2/util/PropsUtil.java
+++ src/main/java/org/smart4j/chapter2/util/PropsUtil.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 
-public class PropsUtil {
+public final class PropsUtil {
 
     private static  final Logger LOGGER= LoggerFactory.getLogger(PropsUtil.class);
 
@@ -40,6 +40,46 @@ public class PropsUtil {
     }
 
 
-    //TODO
+    public static String getString(Properties props,String key,String defaultValue){
+        String value=defaultValue;
+        if(props.contains(key)){
+            value= props.getProperty(key);
+        }
+        return value;
+    }
+
+
+    /**
+     * 获取字符型属性（默认值为空字符串）
+     */
+    public static String getString(Properties props,String key){
+        return getString(props,key,"");
+    }
+
+
+    public static int getInt(Properties props,String key,int defaultValue){
+        int value=defaultValue;
+        if(props.contains(key)){
+            value= CastUtil.castInt(props.getProperty(key));
+        }
+        return value;
+    }
+
+    public static int getInt(Properties props,String key){
+        return getInt(props,key,0);
+    }
+
+
+    public static boolean getBoolean(Properties props,String key,Boolean defaultValue){
+        Boolean value=defaultValue;
+        if(props.contains(key)){
+            value= CastUtil.castBoolean(props.getProperty(key));
+        }
+        return value;
+    }
+
+    public static boolean getBoolean(Properties props,String key){
+        return getBoolean(props,key,false);
+    }
 
 }
