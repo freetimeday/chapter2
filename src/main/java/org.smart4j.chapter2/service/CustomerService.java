170e8e5e520b05f5ded5ea2d13e5e4ddc4512527
freetimeday <836231173@qq.com>
2021-05-25 23:26:08 +0800

    test2

--- src/main/java/org.smart4j.chapter2/service/CustomerService.java
+++ src/main/java/org.smart4j.chapter2/service/CustomerService.java
@@ -16,55 +16,10 @@ public class CustomerService {
     private static  final Logger LOGGER= LoggerFactory.getLogger(CustomerService.class);
 
 
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-   static{
-       Properties conf= PropsUtil.loadProps("config.properties");
-       DRIVER=conf.getProperty("jdbc.driver");
-       URL=conf.getProperty("jdbc.url");
-       USERNAME=conf.getProperty("jdbc.username");
-       PASSWORD=conf.getProperty("jdbc.password");
-   }
-
-
-
 
     public List<Customer> getCustomerList(String keyword){
-        Connection conn=null;
-        try {
-            List<Customer> customerList=new ArrayList<Customer>();
             String sql="SELECT * FROM CUSTOMER";
-            conn= DriverManager.getConnection(URL,USERNAME,PASSWORD);
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                Customer customer=new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-                return customerList;
-        } catch (SQLException e) {
-           LOGGER.error("execute sql failure",e);
-        }finally {
-            if(conn!=null){
-                try{
-                    conn.close();
-                }catch (SQLException e){
-                    LOGGER.error("close connection failure",e);
-                }
-            }
-        }
-
-
-        return null;
+            return DatabaseHelper.queryEntityList(Customer.class,sql);
     }
 
     public Customer getCustomer(long id){
